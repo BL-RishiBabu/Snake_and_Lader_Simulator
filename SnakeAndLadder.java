115c28f50e388488be4bf73273cfd6322a1189d5
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-05-12 12:05:08 +0530

    UC3: Check for options No Play, Ladder, or Snake after die roll

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,11 +1,39 @@
+import java.util.Random;
+
 public class SnakeAndLadder {
     
-    public static final int START_POSITION = 0;
+    public static final int NO_PLAY = 0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Simulator");
-        int playerPosition = START_POSITION;
 
-        System.out.println("The game starts with a single player at position: " + playerPosition);
+        int playerPosition = 0;
+        Random random = new Random();
+
+        int dieRoll = random.nextInt(6) + 1;
+        System.out.println("Die Rolled: " + dieRoll);
+
+        int option = random.nextInt(3);
+
+        switch (option) {
+            case NO_PLAY -> {
+                System.out.println("Option: NO PLAY. Player stays at " + playerPosition);
+            }
+            case LADDER -> {
+                playerPosition += dieRoll;
+                System.out.println("Option: LADDER! Player moves ahead to " + playerPosition);
+            }
+            case SNAKE -> {
+                playerPosition -= dieRoll;
+                if (playerPosition < 0) {
+                    playerPosition = 0;
+                }
+                System.out.println("Option: SNAKE! Player moves behind to " + playerPosition);
+            }
+        }
+
+        System.out.println("Final Position for this turn: " + playerPosition);
     }
 }
