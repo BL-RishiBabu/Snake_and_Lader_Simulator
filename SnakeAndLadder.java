f7fc90a648d158497da0f7b936151707de430e68
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-05-12 11:54:20 +0530

    UC1: Initialize Snake and Ladder game with single player at position 0

---
+++ SnakeAndLadder.java
@@ -0,0 +1,11 @@
+public class SnakeAndLadder {
+    
+    public static final int START_POSITION = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Simulator");
+        int playerPosition = START_POSITION;
+
+        System.out.println("The game starts with a single player at position: " + playerPosition);
+    }
+}
