aaaf61a9ca11b0cdaee18dc09a6d5c3bc59dbdef
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-05-12 12:03:26 +0530

    UC2: Roll the die to get a random number between 1 and 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,11 +1,18 @@
+import java.util.Random;
+
 public class SnakeAndLadder {
     
     public static final int START_POSITION = 0;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Simulator");
+
         int playerPosition = START_POSITION;
 
-        System.out.println("The game starts with a single player at position: " + playerPosition);
+        Random random = new Random();
+        
+        int dieRoll = random.nextInt(6) + 1;
+
+        System.out.println("The player rolled a: " + dieRoll);
     }
 }
