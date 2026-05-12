7c775a8d26a953a11ebc1fc6b5a790fefb9f1338
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-05-12 12:12:58 +0530

    UC4: Repeat game logic until player reaches winning position 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,38 +2,37 @@ import java.util.Random;
 
 public class SnakeAndLadder {
     
+    public static final int START_POSITION = 0;
+    public static final int WINNING_POSITION = 100;
     public static final int NO_PLAY = 0;
     public static final int LADDER = 1;
     public static final int SNAKE = 2;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Simulator");
-
-        int playerPosition = 0;
+        int playerPosition = START_POSITION;
         Random random = new Random();
 
-        int dieRoll = random.nextInt(6) + 1;
-        System.out.println("Die Rolled: " + dieRoll);
-
-        int option = random.nextInt(3);
-
-        switch (option) {
-            case NO_PLAY -> {
-                System.out.println("Option: NO PLAY. Player stays at " + playerPosition);
-            }
-            case LADDER -> {
-                playerPosition += dieRoll;
-                System.out.println("Option: LADDER! Player moves ahead to " + playerPosition);
-            }
-            case SNAKE -> {
-                playerPosition -= dieRoll;
-                if (playerPosition < 0) {
-                    playerPosition = 0;
-                }
-                System.out.println("Option: SNAKE! Player moves behind to " + playerPosition);
-            }
+        while (playerPosition < WINNING_POSITION) {
+            int dieRoll = random.nextInt(6) + 1;
+            int option = random.nextInt(3);
+            switch (option) {
+                case NO_PLAY:
+                    break;
+                    
+                case LADDER:
+                    playerPosition += dieRoll;
+                    break;
+                    
+                case SNAKE:
+                    playerPosition -= dieRoll;
+                    if (playerPosition < 0) {
+                        playerPosition = 0;
+                    }
+                    break;
+            }   
+            System.out.println("Current Position: " + playerPosition);
         }
-
-        System.out.println("Final Position for this turn: " + playerPosition);
+        System.out.println("\nCongratulations! You reached position " + playerPosition + " and won the game!");
     }
 }
